792694c29f6d979729155e6b7003685c10d6fd77
Vova <sanik133@gmail.com>
2022-01-13 15:26:03 +0200

    Define own byte swap function

--- src/pixor.h
+++ src/pixor.h
@@ -5,5 +5,16 @@ typedef int RGBA;
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
 namespace Pixor {
+  template <class T>
+  T byte_swap_32(T x)
+  {
+    byte res[4];
+    byte *src = (byte *) &x;
 
+    for (int i = 0; i < 4; i++) {
+      res[i] = src[3 - i];
+    }
+
+    return *((T *) res);
+  }
 }
