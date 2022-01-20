99c3c8bdf2529dcdc46bf03496781b4140e4a959
Vova <sanik133@gmail.com>
2022-01-20 18:09:12 +0200

    main() now only creates and runs Application

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,7 @@
-#include <gtkmm.h>
-#include <stdio.h>
-#include <fstream>
-#include "main_window.h"
-#include "png.h"
-#include "image_area.h"
+#include "application.h"
 
 int main(int argc, char* argv[])
 {
-  if (argc == 1) {
-    printf("Usage: pixor <file>\n");
-    return 1;
-  }
-
-  std::ifstream file (argv[1], std::ios::in|std::ios::binary);
-
-  if (!file.is_open()) {
-    printf("cannot open %s (No such file or directory)\n", argv[1]);
-    return 1;
-  }
-
-  auto image = std::shared_ptr<Pixor::Image>((Pixor::Image *) Pixor::decode_png(file));
-  image->print_image_info();
-
-  file.close();
-
-  auto app = Gtk::Application::create("org.gtkmm.examples.base");
-  MainWindow window(image);
-
-  return app->run(window);
+  auto application = Application::create();
+  return application->run(argc, argv);
 }
