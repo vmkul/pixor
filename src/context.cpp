e1acb0372e82d7498f377bd6f66e66e93d59f7ad
Vova <sanik133@gmail.com>
2022-01-27 21:09:55 +0200

    Now Pattern can be created from Context, which aids in drawing different shapes

--- src/context.cpp
+++ src/context.cpp
@@ -1,43 +1,11 @@
-#include "math.h"
 #include "context.h"
 #include "debug.h"
 
 using namespace Pixor;
 
-bool Context::coord_in_bounds(point p)
+std::vector<point> Pixor::approx_line(point p1, point p2)
 {
-  return (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height);
-}
-
-void Context::set_source_rgba(RGBA color)
-{
-  source_color = color;
-}
-
-void Context::set_pixel(point coord, RGBA value)
-{
-  pixel_data[coord.y * width + coord.x] = value;
-}
-
-void Context::set_pixel_safe(point coord, RGBA value)
-{
-  if (!coord_in_bounds(coord)) return;
-  set_pixel(coord, value);
-}
-
-RGBA Context::get_pixel(point coord)
-{
-  return pixel_data[coord.y * width + coord.x];
-}
-
-RGBA Context::get_pixel_safe(point coord, RGBA default_value)
-{
-  if (!coord_in_bounds(coord)) return default_value;
-  return get_pixel(coord);
-}
-
-void Context::draw_line(point p1, point p2, int line_width)
-{
+  std::vector<point> res;
   point from;
   point to;
   int dx = std::abs(p1.x - p2.x);
@@ -66,18 +34,97 @@ void Context::draw_line(point p1, point p2, int line_width)
   dx = to.x - from.x;
   dy = to.y - from.y;
   float k = dy / (float) dx;
-  Pattern drawing_pattern = Pattern::make_square(line_width, &source_color);
 
   for (int i = 0; i <= dx; i++) {
     float y_val = i * k + from.y;
     float rounded = std::round(y_val);
 
     if (y_fun) {
-      drawing_pattern.draw_onto(*this, {(int) rounded, i + from.x});
+      res.push_back({(int) rounded, i + from.x});
     } else {
-      drawing_pattern.draw_onto(*this, {i + from.x, (int) rounded});
+      res.push_back({i + from.x, (int) rounded});
     }
   }
+
+  return res;
+}
+
+std::vector<point> Pixor::approx_circle(int radius)
+{
+  std::vector<point> res;
+
+  for (int x = -radius; x <= radius; x++) {
+    int y = (int) std::round(std::sqrt(std::pow(radius, 2) - std::pow(x, 2)));
+
+    res.push_back({x + radius, y + radius});
+    res.push_back({x + radius, -y + radius});
+  }
+
+  return res;
+}
+
+bool Context::coord_in_bounds(point p)
+{
+  return (p.x >= 0 && p.x < width && p.y >= 0 && p.y < height);
+}
+
+void Context::set_source_pattern(std::shared_ptr<Pattern> pattern)
+{
+  source_pattern = pattern;
+}
+
+void Context::set_source_rgba(RGBA color)
+{
+  source_color = color;
+}
+
+void Context::set_pixel(point coord, RGBA value)
+{
+  pixel_data[coord.y * width + coord.x] = value;
+}
+
+void Context::set_pixel_safe(point coord, RGBA value)
+{
+  if (!coord_in_bounds(coord)) return;
+  set_pixel(coord, value);
+}
+
+RGBA Context::get_pixel(point coord)
+{
+  return pixel_data[coord.y * width + coord.x];
+}
+
+RGBA *Context::get_pixel_ptr(point coord)
+{
+  return pixel_data + coord.y * width + coord.x;
+}
+
+RGBA Context::get_pixel_safe(point coord, RGBA default_value)
+{
+  if (!coord_in_bounds(coord)) return default_value;
+  return get_pixel(coord);
+}
+
+void Context::draw_pattern(std::vector<point> &points, Pattern &p)
+{
+  for (const auto &point : points) {
+    p.draw_onto(*this, point);
+  }
+}
+
+void Context::draw_line(point p1, point p2, int line_width)
+{
+  auto points = approx_line(p1, p2);
+  auto drawing_pattern = Pattern::make_square(line_width, &source_color);
+  draw_pattern(points, *drawing_pattern);
+}
+
+void Context::draw_line_with_pattern(point p1, point p2)
+{
+  if (!source_pattern) return;
+
+  auto points = approx_line(p1, p2);
+  draw_pattern(points, *source_pattern);
 }
 
 void Pattern::draw_onto(Context &context, point center)
--- src/context.h
+++ src/context.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <memory>
 #include <vector>
+#include <cmath>
+#include <cstring>
 #include "pixor.h"
+#include "debug.h"
 
 namespace Pixor {
 
@@ -10,8 +13,14 @@ struct point {
   int y;
 };
 
+std::vector<point> approx_circle(int radius);
+std::vector<point> approx_line(point p1, point p2);
+
+class Pattern;
+
 class Context {
   std::shared_ptr<byte[]> bitmap;
+  std::shared_ptr<Pattern> source_pattern;
   RGBA *pixel_data;
   int width;
   int height;
@@ -25,24 +34,58 @@ public:
   {
     pixel_data = (RGBA *) bitmap.get();
   }
+
+  Context(int width, int height) :
+    bitmap(new byte[width * height * 4]),
+    width(width),
+    height(height)
+  {
+    pixel_data = (RGBA *) bitmap.get();
+
+    for (int i = 0; i < width * height * 4; i++) {
+      bitmap[i] = 0;
+    }
+  }
+
+  Context(const Context &context) :
+    width(context.get_width()),
+    height(context.get_height())
+  {
+    int bitmap_size = width * height * 4;
+    byte *bitmap_data = new byte[bitmap_size];
+
+    memcpy(bitmap_data, context.get_target_bitmap().get(), bitmap_size);
+
+    pixel_data = (RGBA *) bitmap_data;
+    bitmap.reset(bitmap_data);
+  }
+
+  int get_width() const {return width;}
+  int get_height() const {return height;}
   bool coord_in_bounds(point p);
+  void set_source_pattern(std::shared_ptr<Pattern> pattern);
   void set_source_rgba(RGBA color);
   void set_pixel(point coord, RGBA value);
   void set_pixel_safe(point coord, RGBA value);
   RGBA get_pixel(point coord);
+  RGBA *get_pixel_ptr(point coord);
   RGBA get_pixel_safe(point coord, RGBA default_value = 0);
+  void draw_pattern(std::vector<point> &points, Pattern &p);
   void draw_line(point p1, point p2, int line_width);
-  std::shared_ptr<byte[]> get_target_bitmap() {return bitmap;}
+  void draw_line_with_pattern(point p1, point p2);
+  const std::shared_ptr<byte[]> get_target_bitmap() const {return bitmap;}
 };
 
 
 class Pattern {
+  Context source_context;
   std::vector<std::vector<RGBA *>> bit_pattern;
   int width;
   int height;
 
 public:
-  Pattern(std::vector<std::vector<RGBA *>> &bit_pattern) :
+  Pattern(std::vector<std::vector<RGBA *>> bit_pattern) :
+    source_context(0, 0),
     bit_pattern(bit_pattern)
   {
     if (bit_pattern.size() == 0) return;
@@ -50,9 +93,29 @@ public:
     height = bit_pattern.size();
   }
 
+  Pattern(Context &source_context, RGBA *mask_color = nullptr) :
+    source_context(source_context),
+    width(source_context.get_width()),
+    height(source_context.get_height())
+  {
+    std::vector<RGBA *> row(width, nullptr);
+    std::vector<std::vector<RGBA *>> pattern(height, row);
+
+    for (int x = 0; x < width; x++) {
+      for (int y = 0; y < height; y++) {
+	RGBA *pixel = this->source_context.get_pixel_ptr({x, y});
+
+	if (mask_color && *pixel == *mask_color) continue;
+	pattern[y][x] = pixel;
+      }
+    }
+
+    bit_pattern = pattern;
+  }
+
   void draw_onto(Context &context, point center);
 
-  static Pattern make_square(int side, RGBA *color)
+  static std::shared_ptr<Pattern> make_square(int side, RGBA *color)
   {
     std::vector<std::vector<RGBA *>> pattern;
 
@@ -66,8 +129,33 @@ public:
       pattern.push_back(row);
     }
 
-    return Pattern(pattern);
+    return std::make_shared<Pattern>(pattern);
   }
+
+  static std::shared_ptr<Pattern> make_circle(int radius, RGBA *color)
+  {
+    int side_len = radius * 2 + 1;
+    Context source_context(side_len, side_len);
+    auto circle_points = approx_circle(radius);
+    const point *prev_point = nullptr;
+
+    source_context.set_source_rgba(*color);
+
+    // TODO: The way circle points are currently connected is not entirely correct
+    for (const auto &point : circle_points) {
+      if (!prev_point) {
+	prev_point = &point;
+	continue;
+      }
+
+      source_context.draw_line(*prev_point, point, 1);
+      prev_point = &point;
+    }
+
+    RGBA mask = 0;
+    return std::make_shared<Pattern>(source_context, &mask);
+  }
+
 };
 
 }
